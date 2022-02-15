a6446f8d2c9b73d8c2f269442ed2fd4000f156a4
NashTheFlash11 <71734964+NashTheFlash11@users.noreply.github.com>
2022-02-15 18:13:14 -0500

    Add files via upload

--- Bob.js
+++ Bob.js
@@ -1,25 +1,33 @@
-class Bob{
-    constructor(x, y, radius){
-        var options={
-            isStatic: false,
-            restitution: 0.3,
-            friction: 0.5,
-            density: 1.2
-        }
-        this.body = Bodies.circle(x, y, radius/2, options);
-        this.radius = radius;
-        World.add(world, this.body);
-    }
+class bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+		World.add(world, this.body);
 
-    display() {
-        var pos = this.body.position;
-        var angle = this.body.angle;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        fill("pink");
-        ellipseMode(CENTER);
-        ellipse(0,0, this.radius);
-        pop();
-    }
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
